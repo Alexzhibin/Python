a8dd1d4bd64a940bd90e39567847a630fb171c19
Alexzhibin <mp43ily@gmail.com>
2015-09-02 16:25:46 -0400

    Create Gradient_Descent_my_version.py

---
+++ Machine_Learning/Algorithm/Gradient_Descent_my_version.py
@@ -0,0 +1,58 @@
+#Part I Create loss/cost function 
+from numpy import *
+x = Symbol('x')
+y = Symbol('y')
+m = Symbol('m')
+b = Symbol('b')
+random.seed(10)
+points = random.random((50,2))
+# y = mx + b
+# m is slope, b is y-intercept
+def compute_error_for_line_given_points(b, m):
+    totalError = 0
+    for i in range(0, len(points)):
+        totalError += (y - (m * x + b)) ** 2
+    return totalError / (2*len(points))
+
+cost_b = compute_error_for_line_given_points(b,m)
+print cost_b
+
+#Devirative 
+from sympy import *
+import numpy as np
+x = Symbol('x')
+y = Symbol('y')
+m = Symbol('m')
+b = Symbol('b')
+devirative_b = diff((-b - m*x + y)**2/2,b)
+devirative_m = diff((-b - m*x + y)**2/2,m)
+x = points[0,0]
+y = points[0,1]
+m = 0.01
+b = 0.1
+devirative_b = b + m*x - y
+devirative_m = -x*(-b - m*x + y)
+print devirative_b
+print devirative_m
+
+#Make the Devirative minimum equal to zero 
+learningrate=0.01
+current_m = 0.1
+current_b= 0.1
+start_x = points[0,0]
+start_y = points[0,1]
+
+for i in range(len(points)):
+        if current_m==current_b==0:
+            print "break"
+            break
+        else:       
+            start_x   =  points[i,0]
+            start_y   =  points[i,1]
+            devirative_b = current_b + current_m*start_x - start_y
+            devirative_m = -start_x*(-current_b - current_m*start_x + start_y)
+            current_m -= learningrate*devirative_m
+            current_b -= learningrate*devirative_b
+
+print [current_m,current_b]    
+        
